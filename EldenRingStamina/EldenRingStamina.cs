b5c2fbb1eb1f5c2b02ba59fd29cec85f1e073f69
Ben Ten <b.m.steponavicius@gmail.com>
2023-06-29 05:58:51 +0300

    initial commit

---
+++ EldenRingStamina/EldenRingStamina.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace EldenRingStamina
+{
+    [BepInPlugin("animegif.eldenring_stamina", "Elden Ring Stamina", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class EldenRingStamina : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("animegif.eldenring_stamina");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Player), "UseStamina")]
+        public static class EldenRing_UseStamina_Patch
+        {
+            private static void Prefix(ref Player __instance, ref float v)
+            {
+                if (__instance.IsSensed() || __instance.IsTargeted()) {
+                    return;
+                }
+                v = 0;
+            }
+        }
+    }
+}
